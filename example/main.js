74b303934361565c1580af6f8e9219a869d62d74
James Mintram <me@jamesrm.com>
2020-10-06 18:58:31 +0200

    Fixes the sample

--- example/main.js
+++ example/main.js
@@ -11,23 +11,8 @@ const quillTwo = new Quill('#editor-two', {
   },
 });
 
-// const cursorsTwo = quillTwo.getModule('cursors');
+quillTwo.getModule('cursor').setScale(1);
 
-quillTwo.getModule('cursor').setScale(3);
-
-// cursorsTwo.createCursor('cursor', 'User 1', 'black');
-
-// function selectionChangeHandler(cursors) {
-//   return function(range, oldRange, source) {
-//     cursors.moveCursor('cursor', range);
-//   };
-// }
-
-// quillTwo.on('selection-change', selectionChangeHandler(cursorsTwo));
-
-// TODO: This needs to be wired in better
-// document.addEventListener('selectionchange', () => {
-//   // Seems to have a side effect in which it recalulcates is
-//   // selection + thus the caret is updated.
-//   // quillTwo.getSelection();
-// });
+document.addEventListener('selectionchange', () => {
+  quillTwo.getSelection();
+});
